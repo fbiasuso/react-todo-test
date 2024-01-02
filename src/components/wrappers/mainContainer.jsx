c870c728c80892dd502d1ab36112f872891f3cff
Francisco Biasuso <145956931+fbiasuso@users.noreply.github.com>
2024-01-02 01:29:53 +0000

    add feature show tasklist

--- src/components/wrappers/mainContainer.jsx
+++ src/components/wrappers/mainContainer.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 
 import { Form } from "../ui/form";
 import { Button } from "../ui/button";
@@ -7,15 +7,12 @@ import { List, Title } from "../ui";
 export const MainContainer = () =>{
 
     const [task, setTask] = useState("");
-    const [tasksList, setTasksList] = useState({
-        id: "",
-        task: ""
-    });
+    const [tasksList, setTasksList] = useState("");
 
     const handleSubmit = (e)=>{
         e.preventDefault();
         const id = crypto.randomUUID();
-        setTasksList({...tasksList, id:id, task:task})
+        setTasksList([...tasksList, {id:id, task:task}])
         e.target.reset();
         setTask("");
     }
@@ -27,7 +24,7 @@ export const MainContainer = () =>{
     }
 
     return (
-            <main className="bg-dark vh-100 text-light d-flex flex-column justify-content-center align-items-center">
+            <main id="main" className="bg-dark vh-100 text-light d-flex flex-column align-items-center pt-5 overflow-auto">
                 <Title name="TAREAS" type="h1" className="mb-5"/>
                 <div className="w-50 mb-5">
                      <Form onSubmit={(e)=> handleSubmit(e)}>
@@ -36,8 +33,8 @@ export const MainContainer = () =>{
                      </Form>
                 </div>
                 <div>
-                    <Title name="Lista de Tareas" type="h2" />
-                    <List />
+                    <Title name="Lista de Tareas" type="h2" className="mb-3 mt-4" />
+                    <List items={tasksList}/>
                 </div>
             </main>
     )
