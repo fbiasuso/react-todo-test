036678bac76cbbc97829cc1cd0a2e20ade255c31
Francisco Biasuso <145956931+fbiasuso@users.noreply.github.com>
2024-01-02 05:43:09 +0000

    Fix 'no task' message was not displayed when there were no tasks

--- src/components/ui/list.jsx
+++ src/components/ui/list.jsx
@@ -12,7 +12,7 @@ setTasksList([...neWitems])
 let deleteId;
     return (
             <ul className={style}>
-              { items ? items.map((item)=>
+                { items.length !== 0 ? items.map((item)=>
                     <div key={item.id} className="d-flex flex-row justify-content-between mb-2">
                     <li  className="list-group-item text-light bg-dark border border-light border-top-0 border-end-0 border-start-0">
                         <input className="form-check-input me-1" type="checkbox" name="task" id={item.id} />
